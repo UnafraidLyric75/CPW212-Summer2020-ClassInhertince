1451d3556e4b053358dc951a0ddeba4908292c47
John Uthus <john.a.uthus.jr1@gmail.com>
2020-08-12 20:21:01 -0700

    Add project files.

---
+++ Product.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceSample
+{
+    /// <summary>
+    /// Represents a salable object
+    /// </summary>
+    class Product
+    {
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        /// <summary>
+        /// Return a display string that represents current product
+        /// </summary>
+        /// <returns></returns>
+        //public string GetDisplayText()
+        //{
+        //    return $"{Name} costs {Price}";
+        //}
+
+        public virtual string GetDisplayText() => $"{Name} costs {Price}";
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    class Book : Product
+    {
+        public string Isbn { get; set; }
+
+        public string Author { get; set; }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    class Software : Product
+    {
+        /// <summary>
+        /// The version number of the software ex.
+        /// 7.81.36
+        /// </summary>
+        public string Verison { get; set; }
+
+        public string ProductKey { get; set; }
+
+        public override string GetDisplayText()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product()
+            {
+                ProductId= 1,
+                Name = "Gadget",
+                Price = 9.99
+            };
+
+            Book b = new Book()
+            {
+                ProductId = 2,
+                Name = "Murach's C#",
+                Price = 39.99,
+                Author = "Joel Murach",
+                Isbn = "9784651321"
+            };
+
+            Software s = new Software()
+            {
+                ProductId = 3,
+                Name = "Visual Studio 2019",
+                Price = 500,
+                ProductKey = "joe@someemail.com",
+                Verison = "16.7.1"
+            };
+        }
+    }
+}
